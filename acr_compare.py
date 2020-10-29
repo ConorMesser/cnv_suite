28fd89ab35ac147ed7d0a2953cf56ed1b0ab08dd
Conor Messer <conorsmesser@gmail.com>
2020-10-29 13:14:00 -0400

    Updated plots. Converted to using pandas backend. Awaiting minor plot edits

--- acr_compare.py
+++ acr_compare.py
@@ -7,6 +7,8 @@ except ModuleNotFoundError:
 from math import log
 import sys
 
+from plot_acr import plot_acr
+
 STAT_COLUMNS = ['mu.minor', 'sigma.minor', 'mu.major', 'sigma.major']
 
 
@@ -30,23 +32,39 @@ def acr_compare(file_1=None, file_2=None):
     seg2 = seg2.dropna(subset=STAT_COLUMNS).reset_index(drop=True)
 
     # take union of segments to allocate bins
-    bins = get_full_union(seg1, seg2)
+    bins = get_union(seg1, seg2)
 
     # call calc_overlap on each "bin", for both alleles
     # can I assume allele marker (major vs. minor) is same for both inputs? todo
-    overlap_scores = np.asarray([b.get_overlap() for b in bins]).flatten()
-    overlap_weights = np.asarray([b.get_weights() for b in bins]).flatten()
-    weighted_average = np.average(overlap_scores, weights=overlap_weights)
+    bins["major_overlap"] = bins.apply(lambda x: calc_overlap(x["mu.major_1"], x["sigma.major_1"],
+                                                              x["mu.major_2"], x["sigma.major_2"],
+                                                              x['unique']), axis=1)
+    bins["minor_overlap"] = bins.apply(lambda x: calc_overlap(x["mu.minor_1"], x["sigma.minor_1"],
+                                                              x["mu.minor_2"], x["sigma.minor_2"],
+                                                              x['unique']), axis=1)
 
-    lengths = np.asarray([b.get_lengths() for b in bins]).sum(axis=0)
-    length_names = ['overlap', 'unique_1', 'unique_2']
-    length_dict = dict(zip(length_names, lengths))
+    # get average overlap score
+    overlap_score = np.average(np.concatenate([bins['major_overlap'], bins['minor_overlap']]),
+                               weights=np.concatenate([bins['length'], bins['length']]))
+
+    non_overlap_length = int(bins['length_1_unique'].sum()) + int(bins['length_2_unique'].sum())
+    overlap_length = int(bins['length_overlap'].sum())
+
+    # plot files with bins marked
+    plot_acr(seg1, seg2, bins, 'REBC Seg File', 'wolF Seg File')
+
+    # overlap_scores = np.asarray([b.get_overlap() for b in bins]).flatten()
+    # overlap_weights = np.asarray([b.get_weights() for b in bins]).flatten()
+    # weighted_average = np.average(overlap_scores, weights=overlap_weights)
+    #
+    # lengths = np.asarray([b.get_lengths() for b in bins]).sum(axis=0)
+    # length_names = ['overlap', 'unique_1', 'unique_2']
+    # length_dict = dict(zip(length_names, lengths))
 
     # return weighted average
-    return weighted_average, length_dict
+    return overlap_score, non_overlap_length, overlap_length, bins
 
-
-def calc_overlap(mu1, sigma1, mu2, sigma2):
+def calc_overlap(mu1, sigma1, mu2, sigma2, unique):
     """
     Calculate the overlap between two normal distributions, defined by given statistics.
 
@@ -56,13 +74,17 @@ def calc_overlap(mu1, sigma1, mu2, sigma2):
     :param sigma2: std dev of distribution 2
     :return: overlap between two distributions as value -> [0, 1]
     """
+    # check if any values are None (non-overlapping)
+    if unique:
+        return 0
+
+    # check if distributions are equal
+    if mu1 == mu2 and sigma1 == sigma2:
+        return 1
+
     # run builtin method in statistics.NormalDist
     if 'statistics' in sys.modules:
         return NormalDist(mu1, sigma1).overlap(NormalDist(mu2, sigma2))
-
-    # if distributions are equal
-    if mu1 == mu2 and sigma1 == sigma2:
-        return 1
 
     # calculate intersection(s) of the two distributions
     x_intersect = calc_pdf_intersect(mu1, sigma1, mu2, sigma2)
@@ -114,20 +136,29 @@ def calc_pdf_intersect(mu1, sigma1, mu2, sigma2):
 
 ####################
 
-def get_full_union(seg1_df, seg2_df):
-    full_bins = get_union(seg1_df, seg2_df)
-    bins_2 = get_union(seg2_df, seg1_df)
+def get_union(seg1_df, seg2_df):  # todo either initialize df with large size or create from list
+    full_bins = _union_one_sided(seg1_df, seg2_df)
+    bins_2 = _union_one_sided(seg2_df, seg1_df)
 
-    for bin in bins_2:
-        if not bin.unique:
-            continue
+    # take only unique values from bins_2
+    unique_2_df = bins_2.loc[bins_2['unique'] == True]
+
+    # swap stats columns (and 1/2 length columns)
+    l = []
+    for c in unique_2_df.columns.values:
+        if '_1' in c:
+            c = c.replace('1', '2')
         else:
-            bin.switch_stats()
-            full_bins.append(bin)
-    return full_bins
+            c = c.replace('2', '1')
+        l.append(c)
+    unique_2_df.columns = l
+
+    bins = pd.concat([full_bins, unique_2_df], ignore_index=True)
+
+    return bins
 
 
-def get_union(seg1_df: pd.DataFrame, seg2_df: pd.DataFrame):
+def _union_one_sided(seg1_df: pd.DataFrame, seg2_df: pd.DataFrame):
     """
     Gets the union of the segment partitions between the two files.
 
@@ -138,7 +169,7 @@ def get_union(seg1_df: pd.DataFrame, seg2_df: pd.DataFrame):
     :param seg2_df: Dataframe of the second seg file
     :return: union of two seg files as a list of Bin objects
     """
-    bins = []
+    bin_list = []
     for chrom in np.arange(1, 23):  # assumes segments in each chromosome (except for XY)
         segments1 = seg1_df.loc[seg1_df['Chromosome'] == chrom].reset_index(drop=True)
         segments2 = seg2_df.loc[seg2_df['Chromosome'] == chrom].reset_index(drop=True)
@@ -150,17 +181,18 @@ def get_union(seg1_df: pd.DataFrame, seg2_df: pd.DataFrame):
             end = segments1.loc[i]['End.bp']
 
             # create bins for this segment1, updating pointer2 to save computation
-            bin_list, pointer2 = create_bins(start, end, segments2, pointer2, segments1.loc[i][STAT_COLUMNS], chrom)
-            bins += bin_list
+            bin_list, pointer2 = create_bins(start, end, segments2, pointer2, segments1.loc[i][STAT_COLUMNS],
+                                             chrom, bin_list=bin_list)
 
-    return bins
+    bin_df = pd.DataFrame(bin_list)
+    return bin_df
 
 
 def create_bins(start, end, segments2, pointer2, segments1_stats, chrom, bin_list=None):
     """
     Creates bins over this segment defined by start to end, as compared to segments2 df.
 
-    :param chrom:
+    :param chrom: this chromosome
     :param start: starting base pair (of seg1)
     :param end: ending base pair (of seg1)
     :param segments2: dataframe for seg2
@@ -169,7 +201,7 @@ def create_bins(start, end, segments2, pointer2, segments1_stats, chrom, bin_lis
     :param bin_list: list of bins, for recursion
     :return: final list of Bin
     """
-    if not bin_list:
+    if bin_list is None:
         bin_list = []
 
     # exit statement for end of segment2
@@ -181,90 +213,89 @@ def create_bins(start, end, segments2, pointer2, segments1_stats, chrom, bin_lis
 
     if start <= start2:
         if end <= start2:
-            this_bin = Bin(start, end, segments1_stats, None, chrom)  # unique segment
-            bin_list.append(this_bin)
+            bin_list.append(append_bin(start, end, segments1_stats, None, chrom))  # unique segment
             return bin_list, pointer2
         else:
-            this_bin = Bin(start, start2, segments1_stats, None, chrom)  # unique segment
-            bin_list.append(this_bin)
+            bin_list.append(append_bin(start, start2, segments1_stats, None, chrom))  # unique segment
+
             if end < end2:
-                this_bin = Bin(start2, end, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom)
-                bin_list.append(this_bin)
+                bin_list.append(append_bin(start2, end, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom))
                 return bin_list, pointer2
             else:
-                this_bin = Bin(start2, end2, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom)
-                bin_list.append(this_bin)
-                return create_bins(end2, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list)
+                bin_list.append(append_bin(start2, end2, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom))
+                return create_bins(end2, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list=bin_list)
     else:
         if end < end2:
-            this_bin = Bin(start, end, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom)
-            bin_list.append(this_bin)
+            bin_list.append(append_bin(start, end, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom))
             return bin_list, pointer2
         else:
             if start < end2:
-                this_bin = Bin(start, end2, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom)
-                bin_list.append(this_bin)
-                return create_bins(end2, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list)
+                bin_list.append(append_bin(start, end2, segments1_stats, segments2.loc[pointer2][STAT_COLUMNS], chrom))
+                return create_bins(end2, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list=bin_list)
             else:
-                return create_bins(start, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list)
+                return create_bins(start, end, segments2, pointer2 + 1, segments1_stats, chrom, bin_list=bin_list)
 
 
-class Bin:
+def append_bin(start, end, stats1, stats2, chromosome):
+    unique = stats1 is None or stats2 is None
+    length = end - start
+    length_overlap = 0
+    length_1_unique = 0
+    length_2_unique = 0
+
+    if not unique:
+        length_overlap = length
+    elif stats2 is None:
+        length_1_unique = length
+    elif stats1 is None:
+        length_2_unique = length
+
+    bin_dict = {'chromosome': chromosome,
+                'Start.bp': start,
+                'End.bp': end,
+                'unique': unique,
+                'length': length,
+                'length_overlap': length_overlap,
+                'length_1_unique': length_1_unique,
+                'length_2_unique': length_2_unique}
+
+    for key in STAT_COLUMNS:
+        if stats1 is not None:
+            val = stats1[key]
+        else:
+            val = None
+        bin_dict[f'{key}_1'] = val
+
+        if stats2 is not None:
+            val = stats2[key]
+        else:
+            val = None
+        bin_dict[f'{key}_2'] = val
+
+    # major_overlap, minor_overlap = get_overlap(stats1, stats2, unique)
+    # bin_dict['major_overlap'] = major_overlap
+    # bin_dict['minor_overlap'] = minor_overlap
+    #
+    # modDf = df.append(bin_dict, ignore_index=True)  #todo super inefficient
+
+    return bin_dict
+
+
+def get_overlap(stats1, stats2, unique):
     """
-    Bin object defining the statistics and length for one Bin
+    Calculates the overlap for both alleles based on the statistics
+
+    :return: list of major and minor overlap scores
     """
+    if unique:
+        return 0, 0
+    major = calc_overlap(stats1['mu.major'],
+                         stats1['sigma.major'],
+                         stats2['mu.major'],
+                         stats2['sigma.major'])
+    minor = calc_overlap(stats1['mu.minor'],
+                         stats1['sigma.minor'],
+                         stats2['mu.minor'],
+                         stats2['sigma.minor'])
 
-    def __init__(self, start, end, stats1, stats2, chromosome):
-        self.length = int(end) - int(start)
-        self.unique = stats1 is None or stats2 is None
-        self.stats1 = stats1
-        self.stats2 = stats2
-        self.chromosome = chromosome
-
-    def get_overlap(self):
-        """
-        Calculates the overlap for both alleles based on the statistics
-
-        :return: list of major and minor overlap scores
-        """
-        if self.unique:
-            return [0, 0]
-        major = calc_overlap(self.stats1['mu.major'],
-                             self.stats1['sigma.major'],
-                             self.stats2['mu.major'],
-                             self.stats2['sigma.major'])
-        minor = calc_overlap(self.stats1['mu.minor'],
-                             self.stats1['sigma.minor'],
-                             self.stats2['mu.minor'],
-                             self.stats2['sigma.minor'])
-
-        return [major, minor]
-
-    def get_weights(self):
-        """
-        Returns the two weights for this bin
-
-        :return: list of length weights
-        """
-        return [self.length] * 2
-
-    def get_lengths(self):
-        """
-        Returns length in appropriate position of list depending on bin type for summing.
-
-        :return: tuple of (overlap_length, unique_1_length, unique_2_length)
-        """
-        lengths = [0, 0, 0]
-        if not self.unique:
-            lengths[0] = self.length
-        elif self.stats2 is None:
-            lengths[1] = self.length
-        elif self.stats1 is None:
-            lengths[2] = self.length
-
-        return lengths
-
-    def switch_stats(self):
-        store_stat = self.stats1
-        self.stats1 = self.stats2
-        self.stats2 = store_stat
+    return major, minor
--- plot_acr.py
+++ plot_acr.py
@@ -3,65 +3,166 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 
-A = pd.read_csv("3328_WGS.tsv", sep="\t")
 
-Ag = A.groupby("Chromosome")
-chr_ends = Ag["End.bp"].apply(max).cumsum()
-chr_ends[0] = 0
-chr_ends = chr_ends.sort_index()
+def plot_acr(seg1, seg2, bins, seg1_name, seg2_name):
+    min_ylim = 1.2
 
-plt.figure(1); plt.clf()
-ax = plt.axes()
-for chrom, C in Ag:
-    for _, x in C.iterrows():
-        span = x[["Start.bp", "End.bp"]] + chr_ends[chrom - 1]
+    fig, ax = plt.subplots(3, figsize=(20, 16), gridspec_kw={'height_ratios': [25, 25, 4]})
+    seg_dfs = [seg1, seg2]
+    combined_chrom_ends = pd.DataFrame()
 
-        # plot mus
-        ax.plot(
-          span,
-          x["mu.major"]*np.r_[1, 1],
-          color = 'r',
-          linestyle = (0, [1, 1]),
-          linewidth = 3
-        )
-        ax.plot(
-          span,
-          x["mu.minor"]*np.r_[1, 1],
-          color = 'b',
-          linestyle = (1, [1, 1]),
-          linewidth = 3
-        )
-        # draw solid line across each dotted segment for clarity
-        ax.plot(
-          span,
-          x["mu.major"]*np.r_[1, 1],
-          color = 'r',
-          linewidth = 0.5
-        )
-        ax.plot(
-          span,
-          x["mu.minor"]*np.r_[1, 1],
-          color = 'b',
-          linewidth = 0.5
-        )
+    for i in range(2):
+        Ag = seg_dfs[i].groupby("Chromosome")
+        chr_ends = Ag["End.bp"].apply(max)
+        chr_ends[0] = 0
+        chr_ends = chr_ends.sort_index()
 
-        # plot sigmas
-        ax.add_patch(patches.Rectangle(
-          [span[0], x["mu.major"] - x["sigma.major"]],
-          span[1] - span[0], 2*x["sigma.major"],
-          color = "r",
-          alpha = 0.5,
-          linewidth = 0
-        ))
-        ax.add_patch(patches.Rectangle(
-          [span[0], x["mu.minor"] - x["sigma.minor"]],
-          span[1] - span[0], 2*x["sigma.minor"],
-          color = "b",
-          alpha = 0.5,
-          linewidth = 0
-        ))
+        combined_chrom_ends = combined_chrom_ends.append(chr_ends, ignore_index=True)
 
-    ax.axvline(chr_ends[chrom], linestyle = ":", color = 'k')
+    combined_chrom_ends = combined_chrom_ends.max(axis=0).cumsum()
 
-ax.set_xlim([0, chr_ends.iloc[-1]])
-plt.xticks((chr_ends.iloc[1:].values + chr_ends[:-1].values)/2, chr_ends.iloc[1:].index)
+    for i in range(2):
+        Ag = seg_dfs[i].groupby("Chromosome")
+        for chrom, C in Ag:
+            for _, x in C.iterrows():
+                span = x[["Start.bp", "End.bp"]] + combined_chrom_ends[chrom - 1]
+
+                # plot mus
+                ax[i].plot(
+                  span,
+                  x["mu.major"]*np.r_[1, 1],
+                  color = 'r',
+                  linestyle = (0, [1, 1]),
+                  linewidth = 3
+                )
+                ax[i].plot(
+                  span,
+                  x["mu.minor"]*np.r_[1, 1],
+                  color = 'b',
+                  linestyle = (1, [1, 1]),
+                  linewidth = 3
+                )
+                # draw solid line across each dotted segment for clarity
+                ax[i].plot(
+                  span,
+                  x["mu.major"]*np.r_[1, 1],
+                  color = 'r',
+                  linewidth = 0.5
+                )
+                ax[i].plot(
+                  span,
+                  x["mu.minor"]*np.r_[1, 1],
+                  color = 'b',
+                  linewidth = 0.5
+                )
+
+                # plot sigmas
+                ax[i].add_patch(patches.Rectangle(
+                  [span[0], x["mu.major"] - x["sigma.major"]],
+                  span[1] - span[0], 2*x["sigma.major"],
+                  color = "r",
+                  alpha = 0.5,
+                  linewidth = 0
+                ))
+                ax[i].add_patch(patches.Rectangle(
+                  [span[0], x["mu.minor"] - x["sigma.minor"]],
+                  span[1] - span[0], 2*x["sigma.minor"],
+                  color = "b",
+                  alpha = 0.5,
+                  linewidth = 0
+                ))
+
+            ax[i].axvline(combined_chrom_ends[chrom], linestyle = ":", color = 'k')
+
+            ax[i].set_xlim([0, combined_chrom_ends.iloc[-1]])
+            # ax[i].set_ylim(ensure_min_ylim(ax[i].get_ylim(), min_ylim))
+
+    ax[0].set_xticks([])
+    ax[1].set_xticks((combined_chrom_ends.iloc[1:].values + combined_chrom_ends[:-1].values) / 2)
+    ax[1].set_xticklabels(combined_chrom_ends.iloc[1:].index)
+    ax[0].tick_params(labelsize=10)
+    ax[1].tick_params(labelsize=10)
+
+    ax[0].set_ylabel(seg1_name)
+    ax[1].set_ylabel(seg2_name)
+
+    scores = []
+    x_heatmap_mat = []
+    bin_line_artists = []
+    prev_end = -100000000
+    # sort bins by chromosome and start.bp
+    bins.sort_values(['chromosome', 'Start.bp'], inplace=True)
+    bins.reset_index(inplace=True)
+
+    # grey dashed line for each bin (how to notate which file when non-overlapping: color?)
+    for ind in bins.index:  # vectorize todo
+        bin_chrom = bins['chromosome'][ind]
+        bin_start = bins['Start.bp'][ind] + combined_chrom_ends[bin_chrom - 1]
+        bin_end = bins['End.bp'][ind] + combined_chrom_ends[bin_chrom - 1]
+
+        if bins['unique'][ind]:
+            if bins['length_1_unique'][ind] > 0:
+                color = "#3D3D8C"
+                zorder = 0.5
+            else:
+                color = "#C94643"
+                zorder = 0.5
+        else:
+            color = "black"
+            zorder = 0.6
+
+        bin_line_artists.append(ax[1].axvline(bin_end, ymin=0, ymax=2.055, c=color, clip_on=False, linewidth=0.5, alpha=0.5, zorder=zorder))
+
+        if (bin_start - prev_end) > 100:
+            bin_line_artists.append(ax[1].axvline(bin_start, ymin=0, ymax=2.055, c=color, clip_on=False, linewidth=0.5, alpha=0.5, zorder=zorder))
+
+            x_heatmap_mat.append(bin_start)
+            if not prev_end < 0:
+                scores.append(-1)
+
+        x_heatmap_mat.append(bin_end)
+        score = (bins['major_overlap'][ind] + bins['minor_overlap'][ind]) / 2  # average of two allelic scores
+        scores.append(score)
+
+        prev_end = bin_end
+
+    # heatmap with overlap score for each bin
+    y_heatmap_mat = [[0] * len(x_heatmap_mat), [1] * len(x_heatmap_mat)]
+    x_heatmap_mat = [x_heatmap_mat, x_heatmap_mat]
+    scores = [scores]
+
+    ax[2].pcolormesh(x_heatmap_mat, y_heatmap_mat, scores, cmap='RdGy')  # 2-D irregularly sized array
+    ax[2].set_xticks([])
+    ax[2].set_yticks([])
+    # ax[2].set_frame_on = False
+    ax[2].set_ylabel('Comparison Score')
+
+    ax[0].set_ylim(ensure_min_ylim(ax[0].get_ylim(), min_ylim))
+    ax[1].set_ylim(ensure_min_ylim(ax[1].get_ylim(), min_ylim))
+    # sm = plt.cm.ScalarMappable(cmap='binary', norm=plt.Normalize(vmin=0, vmax=1))
+    # sm._A = []
+    # plt.colorbar(sm, ax=ax[2])
+    fig.tight_layout()
+
+    fig.savefig('full_plot.png')
+
+    ninety_quartile_1 = bins['mu.major_1'].quantile(0.9, interpolation='lower')
+    ninety_quartile_2 = bins['mu.major_2'].quantile(0.9, interpolation='lower')
+    ax[0].set_ylim(ensure_min_ylim([0, ninety_quartile_1], min_ylim))
+    ax[1].set_ylim(ensure_min_ylim([0, ninety_quartile_2], min_ylim))
+
+    bin_line_artists = np.asarray(bin_line_artists)
+    change_to_gray = lambda x: x.set_color('gray')
+    np.vectorize(change_to_gray)(bin_line_artists)
+
+    plt.show()
+    fig.savefig('zoom_plot.png')
+
+
+def ensure_min_ylim(lim, min_lim):
+    if lim[0] > 0:
+        lim = (0, lim[1])
+    if lim[1] < min_lim:
+        return lim[0], min_lim
+    else:
+        return lim
