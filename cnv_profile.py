2a1690672bc4794aee17435142a4c15044e6ed89
Conor Messer <conorsmesser@gmail.com>
2022-02-14 14:54:45 +0000

    Add multiple methods of passing in chrom sizes; fix bugs

--- cnv_profile.py
+++ cnv_profile.py
@@ -3,6 +3,8 @@ import numpy as np
 from intervaltree import Interval, IntervalTree
 from collections import namedtuple, deque
 from random import choice
+import re
+import natsort
 
 
 Event = namedtuple("Event", ['type', 'allele', 'cluster_num', 'cn_change'])
@@ -11,16 +13,45 @@ Event = namedtuple("Event", ['type', 'allele', 'cluster_num', 'cn_change'])
 class CNV_Profile:
 
     def __init__(self, num_subclones=3, csize=None, cent_loc=None):
-        if not csize:  # pull in from ref file?
+        if not csize:
             csize = {'1': 249250621, '2': 243199373, '3': 198022430, '4': 191154276, '5': 180915260,
                      '6': 171115067, '7': 159138663, '8': 146364022, '9': 141213431, '10': 135534747,
                      '11': 135006516, '12': 133851895, '13': 115169878, '14': 107349540, '15': 102531392,
                      '16': 90354753, '17': 81195210, '18': 78077248, '19': 59128983, '20': 63025520,
-                     '21': 48129895, '22': 51304566, 'X': 156040895, 'Y': 57227415}
+                     '21': 48129895, '22': 51304566, '23': 156040895, '24': 57227415}
+        elif type(csize) == dict:
+            continue
+        else:
+            if type(csize) == str:
+                if csize[-3:] == 'bed':
+                    columns = ['chr', 'start', 'len']  # three columns if bed file
+                else:
+                    columns = ['chr', 'len']
+                csize_df = pd.read_csv(csize, sep='\t', header=None, names=columns)
+            elif type(csize) == pd.DataFrame:
+                csize_df = csize.copy()
+            else:
+                raise ValueError('csize input must be one of [None, dict, file path, pandas DataFrame]')
+            csize_df.set_index('chr', inplace=True)
+            csize = csize_df.to_dict()['len']
+        
         if not cent_loc:
             cent_loc = {chrom: int(size / 2) for chrom, size in csize.items()}  # todo change
-        self.cent_loc = cent_loc
-        self.csize = csize
+        elif type(cent_loc) == dict:
+            continue
+        else:
+            if type(cent_loc) == str:
+                cent_loc_df = pd.read_csv(cent_loc, sep='\t', header=None, names=['chr', 'pos'])
+            elif type(cent_loc) == pd.DataFrame:
+                cent_loc_df = cent_loc.copy()
+                cent_loc_df.columns = ['chr', 'pos']
+            else:
+                raise ValueError('cent_loc input must be one of [None, dict, file path, pandas DataFrame]')
+            cent_loc_df.set_index('chr', inplace=True)
+            cent_loc = cent_loc_df.to_dict()['pos']
+
+        self.cent_loc = switch_contigs(cent_loc)
+        self.csize = switch_contigs(csize)
 
         self.event_trees = self.init_all_chrom()
         self.phylogeny = Phylogeny(num_subclones)
@@ -193,8 +224,11 @@ class CNV_Profile:
         """
         if not sigma:
             sigma = 1
-        x_coverage_df = pd.read_csv(cov_binned, sep='\t', names=['chrom', 'start', 'end', 'coverage', 'coverage2'],
-                                    dtype={'chrom': str})
+        x_coverage_df = pd.read_csv(cov_binned, sep='\t', names=['chrom', 'start', 'end', 'coverage'],
+                                    dtype={'chrom': str, 'start': int, 'end': int}, header=0)
+        
+        # todo change contigs to [0-9]+ from chr[0-9XY]+ in input file
+        x_coverage_df = switch_contigs(x_coverage_df)
 
         x_coverage_df = x_coverage_df[x_coverage_df['chrom'].isin(self.csize.keys())]
         x_coverage_df['ploidy'] = x_coverage_df.apply(
@@ -209,34 +243,43 @@ class CNV_Profile:
             x_coverage_df['coverage'] = this_chr_coverage
 
         x_coverage_df['cov_adjust'] = np.floor(x_coverage_df['coverage'].values * x_coverage_df['ploidy'].values / 2)
+        x_coverage_df['cov_adjust'] = x_coverage_df['cov_adjust'].astype(int)
 
-        return x_coverage_df[['chrom', 'start', 'end', 'cov_adjust', 'coverage', 'coverage2', 'ploidy']]
+        return x_coverage_df[['chrom', 'start', 'end', 'cov_adjust', 'coverage', 'ploidy']]
 
     def save_coverage_file(self, filename, purity, cov_binned_file, x_coverage=None, sigma=None):
         cov_df = self.generate_coverage(purity, cov_binned_file, x_coverage=x_coverage, sigma=sigma)
-        cov_df.replace(columns={'chrom': 'chr', 'cov_adjust': 'covcorr', 'coverage': 'covraw'}).to_csv(filename, sep='\t', index=False)  # todo is this what we want (column headings)?
+        cov_df.rename(columns={'chrom': 'chr', 'cov_adjust': 'covcorr', 'coverage': 'covraw'}).to_csv(filename, sep='\t', index=False)  # todo is this what we want (column headings)?
 
     def generate_snvs(self, vcf, bed, purity):
-        snv_df = pd.read_csv(vcf, sep='\t', comment='#')
-        bed_df = pd.read_csv(bed, sep='\t', header=['CHROM', 'POS', 'DEPTH'])
+        # todo should check that the vcf header (chrs and chr lengths) match with self.chr
+                
+        snv_df = pd.read_csv(vcf, sep='\t', comment='#', header=None, 
+                     names=['CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT','NA12878'])
+        bed_df = pd.read_csv(bed, sep='\t', header=0, names=['CHROM', 'POS', 'DEPTH'], dtype={'CHROM': str})
+
+        # todo change contigs to [0-9]+ from chr[0-9XY]+ in input file
+        snv_df = switch_contigs(snv_df)
+        bed_df = switch_contigs(bed_df)
+        
         snv_df = snv_df.merge(bed_df, on=['CHROM', 'POS'])
         snv_df['ploidy'] = snv_df.apply(
             lambda x: get_average_ploidy(self.cnv_trees[x['CHROM']], x['POS'], x['POS'] + 1, purity),
             axis=1)
 
         snv_df['maternal_prop'] = snv_df.apply(
-            lambda x: (sorted(self.cnv_trees[x['CHROM']][1][x['POS']])[0].data['cn_change'] - 1) *
-                      purity + (1 - purity) / x['total_ploidy'], axis=1)
+            lambda x: ((sorted(self.cnv_trees[x['CHROM']][1][x['POS']])[0].data.cn_change) * purity +
+                      (1 - purity) ) / x['ploidy'], axis=1)
 
         snv_df['paternal_prop'] = snv_df.apply(
-            lambda x: (sorted(self.cnv_trees[x['CHROM']][0][x['POS']])[0].data['cn_change'] - 1) *
-                      purity + (1 - purity) / x['total_ploidy'], axis=1)
+            lambda x: ((sorted(self.cnv_trees[x['CHROM']][0][x['POS']])[0].data.cn_change) * purity +
+                      (1 - purity) ) / x['ploidy'], axis=1)
 
         snv_df['maternal_present'] = snv_df['NA12878'].apply(lambda x: x[0] == '1')
         snv_df['paternal_present'] = snv_df['NA12878'].apply(lambda x: x[2] == '1')
 
-        snv_df['final_depth'] = np.floor(snv_df['DEPTH'] * snv_df['ploidy'] / 2)
-
+        snv_df['adjusted_depth'] = np.floor(snv_df['DEPTH'] * snv_df['ploidy'] / 2).astype(int)
+        
         # generate phase switch profile
         # chromosome interval trees: False if phase switched
         correct_phase_interval_trees = self.generate_phase_switching()
@@ -244,15 +287,15 @@ class CNV_Profile:
         # calculate alt counts for each SNV
         snv_df['alt_count'] = snv_df.apply(
             lambda x: get_alt_count(x['maternal_prop'], x['paternal_prop'], x['maternal_present'],
-                                    x['paternal_present'], x['final_depth'],
+                                    x['paternal_present'], x['adjusted_depth'],
                                     correct_phase_interval_trees[x['CHROM']][x['POS']].pop().data), axis=1)
-        snv_df['ref_count'] = snv_df.apply(lambda x: x['final_depth'] - x['alt_count'])
+        snv_df['ref_count'] = snv_df['adjusted_depth'] - snv_df['alt_count']
 
         return snv_df, correct_phase_interval_trees
 
     def save_hets_file(self, filename, vcf, bed, purity):
-        vcf_df = self.generate_snvs(vcf, bed, purity)
-        vcf_df.replace(columns={'CHROM': 'CONTIG', 'POS': 'POSITION',
+        vcf_df, _ = self.generate_snvs(vcf, bed, purity)
+        vcf_df.rename(columns={'CHROM': 'CONTIG', 'POS': 'POSITION',
                                 'ref_count': 'REF_COUNT', 'alt_count': 'ALT_COUNT'})[['CONTIG', 'POSITION',
                                                                                       'REF_COUNT', 'ALT_COUNT']].to_csv(filename, sep='\t', index=False)
 
@@ -279,21 +322,20 @@ def get_alt_count(m_prop, p_prop, m_present, p_present, coverage, correct_phase)
         m_present, p_present = p_present, m_present
 
     if m_present and p_present:
-        return coverage
-    elif ~m_present and ~p_present:
-        return 0
+        return coverage   # add noise? as in: np.random.binomial(coverage, 0.999)
+    elif not m_present and not p_present:
+        return 0  # noise?
     elif m_present:
-        alt_count = np.random.binomial(coverage, coverage * m_prop)
+        return np.random.binomial(coverage, m_prop)
     else:
-        alt_count = np.random.binomial(coverage, coverage * p_prop)
-    return alt_count
+        return np.random.binomial(coverage, p_prop)
 
 
 def get_average_ploidy(tree, start, end, purity):
     # need to run over paternal and maternal alleles
     def single_allele_ploidy(allele):
         intervals = allele.envelop(start, end) | allele[start] | allele[end - 1]
-        interval_totals = [(min(i.end, end) - max(i.begin, start)) * (i.data['hscr.a2'] + i.data['hscr.a1']) for i in  # todo
+        interval_totals = [(min(i.end, end) - max(i.begin, start)) * i.data[3] for i in  # todo
                            intervals]
         return np.asarray(interval_totals).sum() / (end - start)
     pat_ploidy = single_allele_ploidy(tree[1])
@@ -398,11 +440,19 @@ class Chromosome:
 class Phylogeny:
 
     def __init__(self, num_subclones):
+        """Class to represent simulated tumor phylogeny
+        
+        :param num_subclones: desired number of subclones
+        :attribute parents: dictionary mapping clones (keys) to their parent clone (values), with 1: None representing truncal branch
+        :attribute ccfs: dictionary mapping clones to their CCF
+        """
         self.num_subclones = num_subclones
         self.parents, self.ccfs = self.make_phylogeny()
 
     def make_phylogeny(self):
-        """Greedy algorithm to assign children clones in correct phylogeny based on random CCFs"""
+        """Greedy algorithm to assign children clones in correct phylogeny based on random CCFs
+        
+        :return: (dict, dict) representing the parent and ccfs dictionaries"""
         ccfs = sorted(np.random.rand(self.num_subclones), reverse=True)
         ccfs = {cluster + 2: ccf for cluster, ccf in enumerate(ccfs)}
         parent_dict = {1: None}
@@ -423,8 +473,11 @@ class Phylogeny:
 
         return parent_dict, ccfs
 
-    def get_lineage(self, subclone_num):
-        node = subclone_num
+    def get_lineage(self, node):
+        """Return lineage for the specified clone
+        
+        :param node: index of desired clone
+        :return: (list, list) representing the clones in the lineage and their respective CCFs"""
         cluster_list = []
 
         while node:
@@ -432,3 +485,34 @@ class Phylogeny:
             node = self.parents[node]
 
         return cluster_list, [self.ccfs[c] for c in cluster_list]
+    
+
+def switch_contigs(input_data):
+    """Return the input data with 'chr' removed from contigs and X/Y changed to 23/24.
+    
+    :param input_data: dict or pd.DataFrame with contig as keys or column
+    :returns: dict or pd.DataFrame with altered contig names"""
+    if type(input_data) == pd.DataFrame:
+        contig_column_names = ['Chr', 'Chromosome', 'Chrom', 'Contig']  # defines possible column names
+        contig_column_names = contig_column_names + [s.lower() for s in contig_column_names] + [s.upper() for s in contig_column_names]  # accounts for all lower/upper-case
+        contig_column_names = contig_column_names + [s + 's' for s in contig_column_names]  # pluralizes column names
+        column_idx = np.where([c in contig_column_names for c in input_data.columns])[0][0]  # find contig column
+        column_label = input_data.columns[column_idx]
+        
+        input_data[column_label] = input_data[column_label].apply(lambda x: re.search('(?<=chr)[0-9XY]+|^[0-9XY]+', x).group())
+        input_data.replace(to_replace={column_label:{'X':'23', 'Y':'24'}}, value=None, inplace=True)
+        input_data.sort_values(column_label, key=natsort.natsort_keygen(), inplace=True)
+        return input_data
+    elif type(input_data) == dict:
+        input_data = {re.search('(?<=chr)[0-9XY]+|^[0-9XY]+', key).group():loc for key, loc in input_data.items()}
+        if 'X' in input_data.keys():
+            input_data['23'] = input_data['X']
+            input_data.pop('X')
+        if 'Y' in input_data.keys():
+            input_data['24'] = input_data['Y']
+            input_data.pop('Y')
+        return input_data
+    else:
+        raise ValueError(f'Only dictionaries and pandas DataFrames supported. Not {type(input_data)}.')
+    
+    
