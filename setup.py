2bd4028302ef5efc643745bc7a8183719b084bc5
Conor Messer <conorsmesser@gmail.com>
2022-11-07 16:45:36 -0500

    Update setup.py with long description (pulled from README) and more specific version numbers.
    Version numbers of dependencies should be analyzed further to allow for flexibility but avoiding bugs from API changes or deprecations.

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ ver_info = sys.version_info
 if ver_info < (3,7,0):
     raise RuntimeError("CNV_Suite requires at least python 3.7")
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='cnv_suite',
-    version="0.1",
-    # package_dir={"": "cnv_suite"},
+    version="0.1.0",
     packages=['cnv_suite.compare', 'cnv_suite.simulate', 'cnv_suite.utils', 'cnv_suite.visualize', 'cnv_suite'],
     entry_points={
         'console_scripts': [
@@ -20,17 +24,19 @@ setup(
     description='Copy Number tools for visualization, simulation, and comparison.',
     author='Conor Messer',
     author_email='cmesser@broadinstitute.org',
-    # long_description = long_description,
-    # long_description_content_type = 'text/markdown',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     install_requires=[
-        'pandas>=1.1.3',
-        'numpy>=1.19.0',
-        'scipy>=1.5.3',
-        'matplotlib>=3.3.2',
-        'plotly>=5.6.0',
-        'intervaltree>=3.1.0',
-        'natsort>=8.1.0',
-        'pandarallel>=1.5.5'
+        'pandas>=1.1.3, <2',
+        'numpy>=1.19.0, <1.23',
+        'scipy>=1.5.3, <1.9',
+        'matplotlib>=3.3.2, <3.6',
+        'plotly>=5.6.0, <6',
+        'intervaltree>=3.1.0, <4',
+        'natsort>=8.1.0, <10',
+        'pandarallel>=1.5.5, <1.8',
+        'tqdm>=4.45',
+        'kaleido>=0.2.1, <0.3'
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
